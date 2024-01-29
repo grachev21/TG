a82b7ae0fc3bb27066dee19b6a3342aa4994b68a
grachev21 <grachev613@gmail.com>
2024-01-29 17:22:16 +0400

    learn_03

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-# We download the token from the .env file
+# We download the token from the ".env" file
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 
@@ -16,9 +16,17 @@ dp = Dispatcher(bot=bot)
 
 # Adding an asynchronous function
 async def cmd_start(message: types.Message):
+    # We send the resulting sticker
+    await message.answer_sticker("CAACAgIAAxkBAAMZZbej8-ODTfurPNBwtqP79q5CLDkAApESAAL88OFKHTn4xvJdjLM0BA")
     # Send a message to the user when they enter start.
     # Passing the username through the f string
     await message.answer(f"{message.from_user.first_name} Добро пожаловать в магазин наклеек")
+
+
+# This function returns the sticker ID that the client sends
+# @dp.message_handler(content_types=["sticker"])
+# async def check_sticker(message: types.Message):
+#     await message.answer(message.sticker.file_id)
 
 
 # This function processes messages from the user
